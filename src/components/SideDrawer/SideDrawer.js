81336800efeb2a4e91f408214039496e37d39fa9
Rasmus Hansson <rasmusfredrikhansson@gmail.com>
2021-02-26 21:02:03 +0100

    Fix translation bug in sidebar, added bold type to special offer

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -13,10 +13,10 @@ return (
     <div className={drawerClasses}>
       <ul>
         <li><Link to="hemstadning" activeClass="active" smooth={true} duration={300} spy={true} offset={-80}>{t('HomeCleaning.1')}</Link></li>
-        <li><Link to="stadprofil" activeClass="active" smooth={true} duration={300} spy={true} offset={-80}>{t('CleaningProfile.1')}</Link></li>
-        <li><Link to="tjanster" activeClass="active" smooth={true} duration={300} spy={true} offset={-80}>{t('Services.1')}</Link></li>
-        <li><Link to="personal" activeClass="active" smooth={true} duration={300} spy={true} offset={-80}>{t('Work.1')}</Link></li>
-        <li><Link to="omoss" activeClass="active" smooth={true} duration={300} spy={true} offset={-80}>{t('About.1')}</Link></li>
+        <li><Link to="stadprofil" activeClass="active" smooth={true} duration={300} spy={true} offset={-80}>{t('HomeCleaning.2')}</Link></li>
+        <li><Link to="tjanster" activeClass="active" smooth={true} duration={300} spy={true} offset={-80}>{t('HomeCleaning.3')}</Link></li>
+        <li><Link to="personal" activeClass="active" smooth={true} duration={300} spy={true} offset={-80}>{t('HomeCleaning.4')}</Link></li>
+        <li><Link to="omoss" activeClass="active" smooth={true} duration={300} spy={true} offset={-80}>{t('HomeCleaning.5')}</Link></li>
       </ul>
     </div>
   )
--- src/components/Top.js
+++ src/components/Top.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
+import './Top.css';
+
 const Top = () => {
   const { t, i18n } = useTranslation()
   return (
